44d9020663acbcc743ae08da052faa08516ce166
Puspak <69215834+phycoding@users.noreply.github.com>
2022-06-08 19:24:58 +0530

    Update Crop_app.py

--- Crop_app.py
+++ Crop_app.py
@@ -24,43 +24,66 @@ fertilizer_dic = {
 
         'Nlow': """The N value of your soil is low.
          Please consider the following suggestions:
-        1. Add sawdust or fine woodchips to your soil: – the carbon in the sawdust/woodchips love nitrogen and will help absorb and soak up and excess nitrogen.
-        2. Plant heavy nitrogen feeding plants : – tomatoes, corn, broccoli, cabbage and spinach are examples of plants that thrive off nitrogen and will suck the nitrogen dry.
-        3. Water – soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving less for your plants to use.
-        4. Sugar – In limited studies, it was shown that adding sugar to your soil can help potentially reduce the amount of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in carbon content.
+        1. Add sawdust or fine woodchips to your soil: – the carbon in the sawdust/woodchips love nitrogen and will 
+                        help absorb and soak up and excess nitrogen.
+        2. Plant heavy nitrogen feeding plants : – tomatoes, corn, broccoli, cabbage and spinach are examples of plants
+                        that thrive off nitrogen and will suck the nitrogen dry.
+        3. Water – soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving 
+                        less for your plants to use.
+        4. Sugar – In limited studies, it was shown that adding sugar to your soil can help potentially reduce the amount 
+                        of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks 
+                        up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in 
+                        carbon content.
         5. Add composted manure to the soil.
         6. Plant Nitrogen fixing plants like peas or beans.
         7. Use NPK fertilizers with high N value.
-        8. Do nothing – It may seem counter-intuitive, but if you already have plants that are producing lots of foliage, it may be best to let them continue to absorb all the nitrogen to amend the soil for your next crops.""",
+        8. Do nothing – It may seem counter-intuitive, but if you already have plants that are producing lots of foliage, it may
+                        be best to let them continue to absorb all the nitrogen to amend the soil for your next crops.""",
 
         'PHigh': """The P value of your soil is high.
         Please consider the following suggestions:
-        1. Avoid adding manure – manure contains many key nutrients for your soil but typically including high levels of phosphorous. Limiting the addition of manure will help reduce phosphorus being added.
-        2. Use only phosphorus-free fertilizer – if you can limit the amount of phosphorous added to your soil, you can let the plants use the existing phosphorus while still providing other key nutrients such as Nitrogen and Potassium. Find a fertilizer with numbers such as 10-0-10, where the zero represents no phosphorous.
-        3. Water your soil – soaking your soil liberally will aid in driving phosphorous out of the soil. This is recommended as a last ditch effort.
+        1. Avoid adding manure – manure contains many key nutrients for your soil but typically including high levels of phosphorous.
+                        Limiting the addition of manure will help reduce phosphorus being added.
+        2. Use only phosphorus-free fertilizer – if you can limit the amount of phosphorous added to your soil, you can let the plants
+                        use the existing phosphorus while still providing other key nutrients such as Nitrogen and Potassium. 
+                        Find a fertilizer with numbers such as 10-0-10, where the zero represents no phosphorous.
+        3. Water your soil – soaking your soil liberally will aid in driving phosphorous out of the soil. This is recommended as a 
+                        last ditch effort.
         4. Plant nitrogen fixing vegetables to increase nitrogen without increasing phosphorous (like beans and peas).
         5. Use crop rotations to decrease high phosphorous levels""",
 
         'Plow': """The P value of your soil is low.
         Please consider the following suggestions:
         1. Bone meal – a fast acting source that is made from ground animal bones which is rich in phosphorous.
-        2. Rock phosphate – a slower acting source where the soil needs to convert the rock phosphate into phosphorous that the plants can use.
-        3. Phosphorus Fertilizers – applying a fertilizer with a high phosphorous content in the NPK ratio (example: 10-20-10, 20 being phosphorous percentage).
+        2. Rock phosphate – a slower acting source where the soil needs to convert the rock phosphate into phosphorous that the plants 
+                        can use.
+        3. Phosphorus Fertilizers – applying a fertilizer with a high phosphorous content in the NPK ratio 
+                        (example: 10-20-10, 20 being phosphorous percentage).
         4. Organic compost – adding quality organic compost to your soil will help increase phosphorous content.
         5. Manure – as with compost, manure can be an excellent source of phosphorous for your plants.
         6. Clay soil – introducing clay particles into your soil can help retain & fix phosphorus deficiencies.
-        7. Ensure proper soil pH – having a pH in the 6.0 to 7.0 range has been scientifically proven to have the optimal phosphorus uptake in plants.
-        8. If soil pH is low, add lime or potassium carbonate to the soil as fertilizers. Pure calcium carbonate is very effective in increasing the pH value of the soil.
-        9. If pH is high, addition of appreciable amount of organic matter will help acidify the soil. Application of acidifying fertilizers, such as ammonium sulfate, can help lower soil pH""",
+        7. Ensure proper soil pH – having a pH in the 6.0 to 7.0 range has been scientifically proven to have the optimal phosphorus uptake 
+                        in plants.
+        8. If soil pH is low, add lime or potassium carbonate to the soil as fertilizers. Pure calcium carbonate is very effective in 
+                        increasing the pH value of the soil.
+        9. If pH is high, addition of appreciable amount of organic matter will help acidify the soil. Application of acidifying fertilizers,
+                        such as ammonium sulfate, can help lower soil pH""",
 
-        'KHigh': """The K value of your soil is high</b>.
+        'KHigh': """The K value of your soil is high.
         Please consider the following suggestions:
-        1. Loosen the soil deeply with a shovel, and water thoroughly to dissolve water-soluble potassium. Allow the soil to fully dry, and repeat digging and watering the soil two or three more times.
-        2. Sift through the soil, and remove as many rocks as possible, using a soil sifter. Minerals occurring in rocks such as mica and feldspar slowly release potassium into the soil slowly through weathering.
-        3. Stop applying potassium-rich commercial fertilizer. Apply only commercial fertilizer that has a '0' in the final number field. Commercial fertilizers use a three number system for measuring levels of nitrogen, phosphorous and potassium. The last number stands for potassium. Another option is to stop using commercial fertilizers all together and to begin using only organic matter to enrich the soil.
-        4. Mix crushed eggshells, crushed seashells, wood ash or soft rock phosphate to the soil to add calcium. Mix in up to 10 percent of organic compost to help amend and balance the soil.
+        1. Loosen the soil deeply with a shovel, and water thoroughly to dissolve water-soluble potassium. Allow the soil to fully dry, and 
+           repeat digging and watering the soil two or three more times.
+        2. Sift through the soil, and remove as many rocks as possible, using a soil sifter. Minerals occurring in rocks such as mica and 
+           feldspar slowly release potassium into the soil slowly through weathering.
+        3. Stop applying potassium-rich commercial fertilizer. Apply only commercial fertilizer that has a '0' in the final number field. 
+           Commercial fertilizers use a three number system for measuring levels of nitrogen, phosphorous and potassium. The last number 
+           stands for potassium. Another option is to stop using commercial fertilizers all together and to begin using only organic matter 
+           to enrich the soil.
+        4. Mix crushed eggshells, crushed seashells, wood ash or soft rock phosphate to the soil to add calcium. Mix in up to 10 percent of 
+           organic compost to help amend and balance the soil.
         5. Use NPK fertilizers with low K levels and organic fertilizers since they have low NPK values.
-        6. Grow a cover crop of legumes that will fix nitrogen in the soil. This practice will meet the soil’s needs for nitrogen without increasing phosphorus or potassium.
+        6. Grow a cover crop of legumes that will fix nitrogen in the soil. This practice will meet the soil’s needs for nitrogen without 
+           increasing phosphorus or potassium.
         """,
 
         'Klow': """The K value of your soil is low.
